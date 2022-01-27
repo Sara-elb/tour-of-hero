c558920218b24e99095215d941570dab01e52911
Sara-elb <93645762+Sara-elb@users.noreply.github.com>
2022-01-27 16:18:43 +0100

    test

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // variable : type = affectation
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+
+  onSelect(hero: Hero) {
+    this.messageService.add('Selection du héro: ' + hero.id + " " + hero.name)
+    // this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    this.selectedHero = hero
+  }
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      heroes => this.heroes = heroes
+    );
+  }
+
+
+
+}
